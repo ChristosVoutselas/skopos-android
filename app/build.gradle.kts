3d4e7c7396c920bc7270a9a1c7a8c539efdd0d7f
christos <voutselas.christos@gmail.com>
2026-09-22 13:54:15 +0200

    as

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,8 @@ android {
     kotlinOptions { jvmTarget = "17" }
     buildTypes { release { isMinifyEnabled = false } }
 }
+
+
 dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.6.8")
